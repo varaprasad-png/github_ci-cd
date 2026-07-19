3b096194369558c06c4975f0983c1b929a9dd0d6
senapathilovaprasad-lgtm <praveensenapathilovaprasad@gmail.com>
2026-07-19 12:00:59 +0530

    unit test case updated

---
+++ tests/test_operators.py
@@ -0,0 +1,10 @@
+from src.mathsoperators import add,sub
+
+def test_add():
+    assert add(2,3) == 5
+    assert add(5,6) == 11
+    assert add(5,6) == 11
+def test_sub():
+    assert sub(2,3) == 5
+    assert sub(5,6) == 11
+    assert sub(5,6) == 11
